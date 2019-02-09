686b3b48934ae6262ace15d3ff39c37fbc04c83b
Stanley Masinde <stanleyloren@gmail.com>
2019-02-09 22:04:53 +0300

    Added a fresh currency converter api

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 "use strict"
 //Register SW later here
 
+
+//first get a list of all currencies
+fetch('https://free.currencyconverterapi.com/api/v6/currencies')
+    .then(function (response) {
+        const currencies = response;
+        console.log(currencies)
+    }).catch(function (err) {
+        console.log(err);
+    })
+
 //init variables
-
-const resultsDisplay = document.querySelector('#results');
-
 function convertCurrency(amount, from, to) {
     from = 'USD' //document.querySelector('#from');
-    to =  'PHP'//document.querySelector('#to');
+    to = 'PHP' //document.querySelector('#to');
     const query = from + '_' + to
-    const url = 'https://free.currencyconverterapi.com/api/v6/convert?q=' +
-        query + '&compact=ultra'
+    const url = `https://free.currencyconverterapi.com/api/v6/convert?q=${query}&compact=ultra`
 
     fetch(url).then(function (response) {
-        console.log(response)
+        const rate = response.json();
+        console.log(rate)
     }).catch(function (err) {
         console.log(err)
     })
 }
 
 //demo call the function
-convertCurrency(12,'USD','PHP');
+convertCurrency(12, 'USD', 'PHP');
