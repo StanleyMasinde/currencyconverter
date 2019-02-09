d422f06d5470e99015974c706f67d9055e957e9b
Stanley Masinde <stanleyloren@gmail.com>
2019-02-10 01:17:47 +0300

    the conversions are now working

--- main.js
+++ main.js
@@ -4,27 +4,62 @@
 
 //first get a list of all currencies
 fetch('https://free.currencyconverterapi.com/api/v6/currencies')
-    .then(function (response) {
-        const currencies = response;
-        console.log(currencies)
-    }).catch(function (err) {
+    .then((response) => {
+        console.log(response)
+        return response.json();
+    })
+    .then(function (responseJson) {
+        let currencies = responseJson.results;
+
+        //loop through the currencies
+        for (const currency in currencies) {
+            const Symbol = currencies[currency].currencySymbol;
+            const Name = currencies[currency].currencyName;;
+            const Id = currencies[currency].id;
+            var node = document.createElement("option");
+            node.setAttribute('value', Id);
+            var textnode = document.createTextNode(`(${Id}) ${Name}`);
+            node.appendChild(textnode);
+            document.getElementById("from").appendChild(node);
+        }
+        for (const currency in currencies) {
+            const Symbol = currencies[currency].currencySymbol;
+            const Name = currencies[currency].currencyName;;
+            const Id = currencies[currency].id;
+            var node = document.createElement("option");
+            node.setAttribute('value', Id);
+            var textnode = document.createTextNode(`(${Id}) ${Name}`);
+            node.appendChild(textnode);
+            document.getElementById("to").appendChild(node);
+        }
+    })
+    .catch((err) => {
         console.log(err);
     })
 
 //init variables
 function convertCurrency(amount, from, to) {
-    from = 'USD' //document.querySelector('#from');
-    to = 'PHP' //document.querySelector('#to');
-    const query = from + '_' + to
+    from = document.querySelector('#from').value;
+    to = document.querySelector('#to').value;
+    amount = document.getElementById('value').value;
+    const query = `${from}_${to}`
     const url = `https://free.currencyconverterapi.com/api/v6/convert?q=${query}&compact=ultra`
 
+
     fetch(url).then(function (response) {
-        const rate = response.json();
-        console.log(rate)
-    }).catch(function (err) {
-        console.log(err)
-    })
+            return response.json();
+        }).then(rates => {
+            let rate = rates[query];
+            const finalResult = rate * amount;
+            if (isNaN(finalResult)) {
+                alert('you need to fill all the fields')
+            } else {
+                document.getElementById('results').innerHTML = finalResult;
+            }
+        })
+        .catch(function (err) {
+            console.log(err)
+        })
 }
 
 //demo call the function
-convertCurrency(12, 'USD', 'PHP');
